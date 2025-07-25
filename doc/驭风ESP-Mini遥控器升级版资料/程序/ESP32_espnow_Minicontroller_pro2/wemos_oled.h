5c692b93adb2d1da6ec64a07e2914e5f460c9cab
550W_Plus <1092910677@qq.com>
2025-07-25 12:35:22 +0800

    添加 驭风ESP-Mini遥控器升级版资料包
    驭风ESP-Mini遥控器升级版资料

---
+++ doc/驭风ESP-Mini遥控器升级版资料/程序/ESP32_espnow_Minicontroller_pro2/wemos_oled.h
@@ -0,0 +1,210 @@
+#ifndef WEMOS_OLED_H
+#define WEMOS_OLED_H
+
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+// SCL GPIO5
+// SDA GPIO4
+#define OLED_RESET 0  // GPIO0
+Adafruit_SSD1306 display(OLED_RESET);
+
+//取64*30 逐行式 顺向高位在前
+static const unsigned char PROGMEM pic1[] = {
+0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,
+0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x03,0xFF,0xC0,0x00,
+0x1F,0xE0,0x00,0x00,0x07,0xFF,0xF0,0x00,0x1F,0xF8,0x00,0x00,0x0F,0xFF,0xFC,0x00,
+0x1F,0xFC,0x00,0x00,0x0F,0xFF,0xFE,0x00,0x1F,0xFE,0x00,0x00,0x0F,0xFF,0xFF,0x00,
+0x1F,0xFF,0x00,0x00,0x0F,0xFF,0xFF,0x80,0x0F,0xFF,0x3F,0xC0,0x0F,0xFF,0x1F,0x80,
+0x07,0xFF,0x7F,0xF8,0x0F,0xE0,0x01,0xC0,0x00,0x07,0x7F,0xFC,0x07,0xE0,0x00,0xC0,
+0x1C,0x03,0x7F,0xFE,0x07,0xC0,0x1C,0xC0,0x3E,0x03,0x7F,0xFE,0x07,0xDC,0x3E,0xC0,
+0x3E,0x03,0x7F,0xFE,0x07,0xDE,0x7E,0x40,0x3E,0x02,0x3F,0xFE,0x07,0xDF,0xFE,0x40,
+0x3E,0x06,0x01,0xFC,0x07,0xC7,0xFE,0x40,0x3E,0x08,0xC0,0x1C,0x0F,0xC7,0xFC,0x40,
+0x1F,0x00,0xF8,0x38,0x0F,0xCF,0xF8,0x40,0x0F,0xFC,0xFF,0x70,0x0F,0x9F,0xF8,0x40,
+0x07,0xE6,0x7F,0xE0,0x0F,0x9F,0xF8,0x40,0x00,0x06,0x3F,0xF8,0x0F,0x9F,0xCC,0x60,
+0x03,0xE6,0x0F,0xF8,0x0F,0x9F,0x06,0x70,0x0F,0xE6,0x0F,0xF8,0x0F,0x0E,0x00,0x3C,
+0x1F,0x8C,0x18,0x30,0x0F,0x00,0x00,0x00,0x0F,0x08,0x00,0x00,0x0E,0x00,0x00,0x00,
+0x0C,0x00,0x00,0x00,0x06,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,
+0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00
+};
+
+#if (SSD1306_LCDHEIGHT != 48)
+#error("Height incorrect, please fix Adafruit_SSD1306.h!");
+#endif
+
+void oleddisplay(){      //WEMOS_OLED扩展板屏幕显示
+  display.clearDisplay();//清屏    
+  display.setCursor(1,23);            
+  display.print("X:");             
+  display.print(data.j1PotX);     
+  display.setCursor(1,35);
+  display.print("Y:");  
+  display.print(data.j1PotY);    
+  display.setCursor(34,23);
+  display.print("X:");             
+  display.print(data.j2PotX);     
+  display.setCursor(34,35);
+  display.print("Y:");  
+  display.print(data.j2PotY); 
+
+  if(!data.buttonLB) display.fillCircle(0, 0, 3, WHITE);
+  if(!data.buttonR2) display.fillCircle(0, 48, 3, WHITE);
+  if(!data.buttonRB) display.fillCircle(64, 0, 3, WHITE);
+  if(!data.buttonR1) display.fillCircle(64, 48, 3, WHITE);  
+  display.invertDisplay((data.tSwitch1?0:1));
+  
+  display.drawLine(0, 14, 64, 14, WHITE);
+  display.setCursor(31,4);display.print(voltage);display.print("V");
+  display.drawRoundRect(3,4,20,7,1,WHITE);
+  display.fillRoundRect(3,4,map(percentage,0,100,0,20),7,1,WHITE);
+  display.fillRect(23,6,2,3,WHITE);
+    
+  display.display();
+}
+
+//在CORE0上运行OLED相关程序（Arduino默认在CORE1上运行）
+/*************************************/
+TaskHandle_t Task1;
+
+  //Task1code: blinks an LED every 1000 ms
+void Task1code( void * pvParameters ){
+  //
+  Serial.print("Task1 running on core ");
+  Serial.println(xPortGetCoreID());
+
+/************************************************/
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);                  //注：有时地址不是0x3C,可能是0x3D等，若屏幕不亮可以试试用wire库中的示例程序扫描地址
+/************************************************/
+  
+  display.setTextColor(WHITE);//开像素点发光
+  display.setTextSize(1);  
+  
+  for(int i=0;i<=18;i++){
+    display.clearDisplay();//清屏
+    display.drawBitmap(0, i, pic1, 64, 30, WHITE); //画出字符对应点阵数据
+    display.display();//开显示
+    delay(20);
+  }
+  for(int i=18;i>=9;i--){
+    display.clearDisplay();//清屏
+    display.drawBitmap(0, i, pic1, 64, 30, WHITE); //画出字符对应点阵数据
+    display.display();//开显示
+    delay(20);    
+  }
+  delay(600);
+
+  for(;;){
+    oleddisplay();//WEMOS_OLED扩展板屏幕显示 
+    delay(10);
+
+/************************************************/
+  //同时按下四键及拨动开关 关 3秒后进行摇杆原点纠偏，此时不要碰摇杆 
+    if (!data.buttonR1&&!data.buttonR2&&!data.buttonLB&&!data.buttonRB&&!data.tSwitch1){
+      delay (2000);
+      if (!data.buttonR1&&!data.buttonR2&&!data.buttonLB&&!data.buttonRB&&!data.tSwitch1){
+        display.clearDisplay();//清屏    
+        display.setCursor(6,6);  display.print("Zero_test"); 
+        display.setCursor(4,22);  display.print("Testing...");    
+        display.display();//开显示
+        delay (1000);
+        
+        display.clearDisplay();//清屏    
+        display.setCursor(6,6);  display.print("Zero_test");    
+        display.setCursor(1,18);            
+        display.print("X:");             
+        display.print(EEPROM.read(1));     
+        display.setCursor(1,30);
+        display.print("Y:");  
+        display.print(EEPROM.read(2));    
+        display.setCursor(34,18);
+        display.print("X:");             
+        display.print(EEPROM.read(3));     
+        display.setCursor(34,30);
+        display.print("Y:");  
+        display.print(EEPROM.read(4));
+        display.display();//开显示
+        delay (2000); 
+      }
+    }
+
+//同时按下四键及拨动开关 开 2秒后进行扫描MAC地址及配对 
+  if (!data.buttonR1&&!data.buttonR2&&!data.buttonLB&&!data.buttonRB&&data.tSwitch1){
+    delay (2000);
+    if (!data.buttonR1&&!data.buttonR2&&!data.buttonLB&&!data.buttonRB&&data.tSwitch1){
+        display.clearDisplay();//清屏    
+        display.setCursor(6,6);  display.print("Scan&Pair"); 
+        display.setCursor(4,22);  display.print("Scaning...");    
+        display.display();//开显示
+//        delay (1000);        
+        display.clearDisplay();//清屏    
+
+        ScanForSlave();
+        // If Slave is found, it would be populate in `slave` variable
+        // We will check if `slave` is defined and then we proceed further
+        manageSlave();
+        if (SlaveCnt > 0) { // check if slave channel is defined 
+          //屏幕空间受限，只显示两个MAC地址 
+          for (int i = 0; i < 2; i++) {   //屏幕空间受限，只显示两个MAC地址
+            display.setCursor(0,8+i*20);
+            
+            // 将MAC地址数组中的值转换为字符串并显示  
+            char macStr[18]; // 假设MAC地址以冒号分隔的形式显示，需要18个字符的缓冲区（包括结尾的'\0'）  
+            for (int j = 0; j < 6; j++) {  
+              sprintf(&macStr[j*3], "%02X:", slaves[i].peer_addr[j]); // 将每个字节转换为两位的十六进制字符串并拼接到macStr中  
+            }  
+            macStr[17] = '\0'; // 结尾的冒号是多余的，需要删除  
+            display.print(macStr); // 显示MAC地址字符串    
+            } 
+        } else {
+          BroadcastMode();
+          display.setCursor(6,6);
+          display.print("No Slave");
+          display.setCursor(4,15);
+          display.print("Broadcast");
+          display.setCursor(0,28);            
+          // 将MAC地址数组中的值转换为字符串并显示  
+          char macStr[18]; // 假设MAC地址以冒号分隔的形式显示，需要18个字符的缓冲区（包括结尾的'\0'）  
+          for (int j = 0; j < 6; j++) {  
+            sprintf(&macStr[j*3], "%02X:", slaves[0].peer_addr[j]); // 将每个字节转换为两位的十六进制字符串并拼接到macStr中  
+          }  
+          macStr[17] = '\0'; // 结尾的冒号是多余的，需要删除  
+          display.print(macStr); // 显示MAC地址字符串                                                 
+        }
+        display.display();//开显示
+        delay (3000); 
+      }
+    }
+  
+  //同时按下四键后变换背光RGB模式 
+    if (!data.j1Button&&!data.j2Button&&!data.buttonLB&&!data.buttonRB){
+      delay (300);
+      if (!data.j1Button&&!data.j2Button&&!data.buttonLB&&!data.buttonRB){        
+        display.clearDisplay();//清屏    
+        display.setCursor(8,16);  display.print("Back_RGB:");
+        display.setCursor(15,28); display.print("Mode:");
+        display.setCursor(47,28); display.print(lightState);     
+        display.display();//开显示
+        delay (1500);
+      }
+    } 
+/************************************************/ 
+
+  } 
+}
+
+void run_OLED_on_Core_0(){
+//create a task that will be executed in the Task1code() function, with priority 1 and executed on core 0
+  xTaskCreatePinnedToCore(
+                    Task1code,   /* Task function. */
+                    "Task1",     /* name of task. */
+                    10000,       /* Stack size of task */
+                    NULL,        /* parameter of the task */
+                    1,           /* priority of the task */
+                    &Task1,      /* Task handle to keep track of created task */
+                    0);          /* pin task to core 0 */                  
+  delay(500); 
+}
+/*************************************/
+#endif 
---
+++ doc/驭风ESP-Mini遥控器升级版资料/程序/旧版程序/ESP32_espnow_Minicontroller_pro/mapdata.h
@@ -0,0 +1,287 @@
+#ifndef MAPDATA_H
+#define MAPDATA_H
+
+#include <EEPROM.h>
+#define EEPROM_SIZE 9
+//int addr = 0;
+
+// TRIGGER BUTTONS
+#define LB 19
+#define RB 25
+
+// LEFT-JOYSTICK
+#define LH 34
+#define LV 35
+#define LS 5
+
+// RIGHT-JOYSTICK
+#define RH 36
+#define RV 32
+#define RS 4
+
+#define R1 2
+#define R2 13
+
+// BATTERY VOLTAGE
+#define ADC 39  //电池ADC
+
+// RGB
+//#define DATA_PIN 17
+
+//OTHERS
+#define BK 23  //拨动开关
+
+int buttons[7] = {R1,R2,LS,RS,LB,RB,BK};
+
+
+const int numberOfPotSamples = 5;     // Number of pot samples to take (to smooth the values)
+const int delayBetweenSamples = 2;    // Delay in milliseconds between pot samples
+
+int LX_read = 0;
+int LY_read = 0;
+int RX_read = 0;
+int RY_read = 0;
+
+byte LX_zero = 127;
+byte LY_zero = 127;
+byte RX_zero = 127;
+byte RY_zero = 127;
+
+int LX_to_send = 0;
+int LY_to_send = 0;
+int RX_to_send = 0;
+int RY_to_send = 0;
+
+bool LY_inverted = false; 
+bool LX_inverted = false; 
+bool RY_inverted = false; 
+bool RX_inverted = false; 
+
+int counter = 0;
+int invert_counter = 0;
+
+float voltage = 3.00;
+int percentage = 0;
+
+void pinmode_pullup(){
+  for (int i = 0; i < 7; i++) {
+    pinMode(buttons[i], INPUT_PULLUP);
+  }
+}
+
+  
+/**************************************************/
+int map_normal(int val, int lower, int middle, int upper, bool reverse)
+{
+  val = constrain(val, lower, upper);
+  if ( val < middle )
+    val = map(val, lower, middle, 0, 127);
+  else
+    val = map(val, middle, upper, 127, 255);
+  return ( reverse ? 255 - val : val );
+}
+
+// Returns a corrected value for a joystick position that takes into account
+// the values of the outer extents and the middle of the joystick range.
+int map_exponential(int val, bool reverse)
+{
+  val = constrain(val, 0, 1023);
+  float cube = ((pow((val - 512),3)/520200) + 258.012) / 2; 
+  return ( reverse ? 255 - cube : cube );
+}
+/**************************************************/
+
+void read_joydata(){
+  int potValues[numberOfPotSamples];
+    for (int i = 0 ; i < numberOfPotSamples ; i++) {
+      potValues[i] = analogRead(LH);
+      delay(delayBetweenSamples);
+    }
+    int potValue = 0;
+    for (int i = 0 ; i < numberOfPotSamples ; i++) {
+      potValue += potValues[i];
+    }
+    potValue = potValue / numberOfPotSamples;
+    LX_read = map(potValue, 0, 4095, 255, 0);
+
+
+    int potValues2[numberOfPotSamples];
+    for (int i = 0 ; i < numberOfPotSamples ; i++) {
+      potValues2[i] = analogRead(LV);
+      delay(delayBetweenSamples);
+    }
+    int potValue2 = 0;
+    for (int i = 0 ; i < numberOfPotSamples ; i++) {
+      potValue2 += potValues2[i];
+    }
+    potValue2 = potValue2 / numberOfPotSamples;
+    LY_read = map(potValue2, 0, 4095, 255, 0);
+
+
+    int potValues3[numberOfPotSamples];
+    for (int i = 0 ; i < numberOfPotSamples ; i++) {
+      potValues3[i] = analogRead(RH);
+      delay(delayBetweenSamples);
+    }
+    int potValue3 = 0;
+    for (int i = 0 ; i < numberOfPotSamples ; i++) {
+      potValue3 += potValues3[i];
+    }
+    potValue3 = potValue3 / numberOfPotSamples;
+    RX_read = map(potValue3, 0, 4095, 255, 0);
+
+
+    int potValues4[numberOfPotSamples];
+    for (int i = 0 ; i < numberOfPotSamples ; i++) {
+      potValues4[i] = analogRead(RV);
+      delay(delayBetweenSamples);
+    }
+    int potValue4 = 0;
+    for (int i = 0 ; i < numberOfPotSamples ; i++) {
+      potValue4 += potValues4[i];
+    }
+    potValue4 = potValue4 / numberOfPotSamples;
+    RY_read = map(potValue4, 0, 4095, 255, 0);
+}
+
+
+void zero_test(){   //摇杆原点纠偏程序
+  Serial.println(" joy_zero_testing... ");
+  read_joydata();
+  delay (300);
+  
+  LX_to_send = map_normal(LX_read, 0, 127, 255, 0);    
+  LY_to_send = map_normal(LY_read, 0, 127, 255, 0);     
+  RX_to_send = map_normal(RX_read, 0, 127, 255, 0);    
+  RY_to_send = map_normal(RY_read, 0, 127, 255, 0);
+       
+  LX_to_send = constrain(LX_to_send,0,255);
+  LY_to_send = constrain(LY_to_send,0,255);
+  RX_to_send = constrain(RX_to_send,0,255);  
+  RY_to_send = constrain(RY_to_send,0,255);
+
+  LX_zero = LX_to_send;
+  LY_zero = LY_to_send;
+  RX_zero = RX_to_send;
+  RY_zero = RY_to_send;
+
+  Serial.println(" Writing in EEPROM... ");
+  delay (300);
+  
+  if( EEPROM.read(1) != LX_zero) EEPROM.write(1, LX_zero);
+  if( EEPROM.read(2) != LY_zero) EEPROM.write(2, LY_zero);
+  if( EEPROM.read(3) != RX_zero) EEPROM.write(3, RX_zero);
+  if( EEPROM.read(4) != RY_zero) EEPROM.write(4, RY_zero);
+  EEPROM.commit();
+
+  Serial.println(" Done... ");
+  Serial.print(" LX_zero: ");Serial.print(EEPROM.read(1));
+  Serial.print(" LY_zero: ");Serial.print(EEPROM.read(2));
+  Serial.print(" RX_zero: ");Serial.print(EEPROM.read(3));
+  Serial.print(" RY_zero: ");Serial.println(EEPROM.read(4));  
+}
+  
+void eeprom_ini(){ 
+  EEPROM.begin(EEPROM_SIZE);
+   
+  if( EEPROM.read(0) != 55){     //判断是否首次使用（新的所有地址貌似是255的值）
+    
+      zero_test();      
+        
+      EEPROM.write(0, 55);
+      EEPROM.commit();
+    }
+
+  LX_zero = EEPROM.read(1);
+  LY_zero = EEPROM.read(2);
+  RX_zero = EEPROM.read(3);
+  RY_zero = EEPROM.read(4);
+  
+}
+
+
+void batteryLevel() {
+  int sensorValue = analogRead(ADC);
+  voltage = sensorValue/4095.0 * 7.27;   //7.23乘数具体数值用万用表实测纠偏    
+  percentage = (voltage-3.2) * 100;
+//  percentage = 2808.3808 * pow(voltage,4) - 43560.9157 * pow(voltage,3) + 252848.5888 * pow(voltage,2)
+//                   - 650767.4615 * voltage + 626532.5703; //这段计算公式参考https://www.youtube.com/watch?v=qKUrXwkr3cc
+if (voltage>4.19) percentage = 100;     //限制到0-100范围
+  if (voltage<3.20) percentage = 0;
+  if (voltage>3.00){
+//    Serial.print("Lipo: ");Serial.print(voltage);Serial.print("V || ");
+//    Serial.print(percentage);Serial.println("%");
+  }
+  
+//  bleGamepad.setBatteryLevel(percentage);
+//  if (percentage < 60) {
+//    Serial.println("LOW battery");
+//    for (uint8_t i = 30; i < 220; i++) {
+//      for (int j = 0; j < 6; j++) {
+//        leds[j] = CHSV( HUE_RED, 255, i);
+//      }
+//      FastLED.show();
+//      delay(15);
+//    }
+//    for (uint8_t i = 220; i > 30; i--) {
+//      for (int j = 0; j < 6; j++) {
+//        leds[j] = CHSV( HUE_RED, 255, i);
+//      }
+//      FastLED.show();
+//      delay(15);
+//    }
+//  }
+}
+
+//摇杆坐标转化为离原点距离
+int joy_distance(float coord_x,float coord_y){
+  int joy_dis=0;
+  joy_dis=sqrt(sq(coord_x)+sq(coord_y));
+  if (joy_dis < 5) joy_dis=0;      //消除一些摇杆误差
+  return joy_dis;
+}
+//摇杆坐标转化为0-360的角度
+int coordinates_to_angle(float coord_x,float coord_y){  
+  int joy_angle=0;
+  if (coord_x>0&&coord_y>0){        //第一象限
+    joy_angle=asin(coord_y/sqrt(sq(coord_x)+sq(coord_y)))*180/3.14159;
+  }
+  if (coord_x<0&&coord_y>0){        //第二象限
+    joy_angle=180-asin(coord_y/sqrt(sq(coord_x)+sq(coord_y)))*180/3.14159;
+  }
+  if (coord_x<0&&coord_y<0){        //第三象限
+    joy_angle=180-asin(coord_y/sqrt(sq(coord_x)+sq(coord_y)))*180/3.14159;  //此时Y坐标为负数
+  }
+  if (coord_x>0&&coord_y<0){        //第四象限
+    joy_angle=360+asin(coord_y/sqrt(sq(coord_x)+sq(coord_y)))*180/3.14159;  //此时Y坐标为负数
+  }
+  return joy_angle;
+}
+//摇杆角度转化为左摇杆LED序号
+int angle_to_lednum1(int angle){ 
+  int led_num=0;
+  if ((0<=angle&&angle<23)||(338<=angle&&angle<360)) led_num=0;
+  if (23<=angle&&angle<68) led_num=7;
+  if (68<=angle&&angle<113) led_num=6;
+  if (113<=angle&&angle<158) led_num=5;
+  if (158<=angle&&angle<203) led_num=4;
+  if (203<=angle&&angle<248) led_num=3;
+  if (248<=angle&&angle<293) led_num=2;
+  if (293<=angle&&angle<338) led_num=1;
+  return led_num;
+}
+//摇杆角度转化为右摇杆LED序号
+int angle_to_lednum2(int angle){ 
+  int led_num=0;
+  if ((0<=angle&&angle<23)||(338<=angle&&angle<360)) led_num=4;
+  if (23<=angle&&angle<68) led_num=3;
+  if (68<=angle&&angle<113) led_num=2;
+  if (113<=angle&&angle<158) led_num=1;
+  if (158<=angle&&angle<203) led_num=0;
+  if (203<=angle&&angle<248) led_num=7;
+  if (248<=angle&&angle<293) led_num=6;
+  if (293<=angle&&angle<338) led_num=5;
+  return led_num;
+}
+
+#endif 
